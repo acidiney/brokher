9e864fea9d166b8d6f703c6b370c2bc9f85de927
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2020-12-18 15:17:36 +0100

    fix: entry point

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { BrokherEnum } from './enums/BrokherEnum'
 
 export class Brokher {
-  async setup (Brokher: BrokherEnum) {
+  static async setup (Brokher: BrokherEnum) {
     const Contract = await import(`./contracts/${Brokher}`)
 
     return new Contract()
